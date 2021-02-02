8c74015e0f50259471de684af27346fc2c103c3c
Gabriel Vidal-Ayrinhac <gabriel.vidal-ayrinhac@epita.fr>
2021-02-02 23:28:52 +0100

    initial commit

---
+++ Explorama/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 1f;
+
+    private CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+        
+        
+        
+        if (Input.GetKey(KeyCode.Z))
+            move += transform.forward;
+        else if (Input.GetKey(KeyCode.S))
+            move -= transform.forward;
+        
+        if (Input.GetKey(KeyCode.D))
+            move += transform.right;
+        else if (Input.GetKey(KeyCode.Q))
+            move -= transform.right;
+
+        cc.Move(move.normalized * speed * Time.deltaTime + Vector3.down);
+    }
+}
